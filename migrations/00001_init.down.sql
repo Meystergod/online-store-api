66d850ae51d3d87396df6f149aa3941b5ecd8e7e
Meystergod <malahov_gleb@mail.ru>
2022-11-05 17:08:14 +0300

    add sql script for database

--- migrations/00001_init.down.sql
+++ migrations/00001_init.down.sql
@@ -1,5 +1,14 @@
 BEGIN;
 
-DROP TABLE IF EXISTS public.currency CASCADE;
+DROP TABLE IF EXISTS public.user CASCADE;
+DROP TABLE IF EXISTS public.car CASCADE;
+DROP TABLE IF EXISTS public.car_model CASCADE;
+DROP TABLE IF EXISTS public.car_brand CASCADE;
+DROP TABLE IF EXISTS public.car_modification CASCADE;
+DROP TABLE IF EXISTS public.product CASCADE;
+DROP TABLE IF EXISTS public.product_category CASCADE;
+DROP TABLE IF EXISTS public.product_discount CASCADE;
+DROP TABLE IF EXISTS public.product_inventory CASCADE;
+DROP TABLE IF EXISTS public.products_for_cars CASCADE;
 
 END;
--- migrations/00001_init.up.sql
+++ migrations/00001_init.up.sql
@@ -9,10 +9,76 @@ SET search_path = public, extensions;
 SET default_tablespace = '';
 SET default_with_oids = FALSE;
 
--- EXTENSIONS --
-
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- TABLES --
+CREATE TABLE public.user
+(
+    id SERIAL PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    created_at TIMESTAMP,
+    visited_at TIMESTAMP,
+    is_admin BOOLEAN NOT NULL
+);
+
+CREATE TABLE public.car_brand
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE public.car
+(
+    id SERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    brand_id INT NOT NULL REFERENCES public.car_brand(id),
+    model TEXT NOT NULL,
+    modification TEXT NOT NULL
+);
+
+CREATE TABLE public.product_category
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE public.product_discount
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE public.product_inventory
+(
+    id SERIAL PRIMARY KEY,
+    quantity INT
+);
+
+CREATE TABLE public.product
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    brand TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price BIGINT NOT NULL,
+    image_id UUID,
+    specifications JSONB,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    category_id INT NOT NULL REFERENCES public.product_category(id),
+    discount_id INT NOT NULL REFERENCES public.product_discount(id),
+    inventory_id INT NOT NULL UNIQUE REFERENCES public.product_inventory(id),
+    CONSTRAINT positive_price CHECK (price > 0)
+);
+
+CREATE TABLE products_for_cars
+(
+    product_id UUID NOT NULL REFERENCES public.product(id),
+    car_id INT NOT NULL REFERENCES public.car(id),
+    CONSTRAINT product_car_pkey PRIMARY KEY (product_id, car_id)
+);
 
 COMMIT;

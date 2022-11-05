0bd590655e407c2701db36a2cd4b1e22cf589cc1
Meystergod <malahov_gleb@mail.ru>
2022-11-05 15:51:34 +0500

    add migrations and database sql script

--- migrations/00001_init.down.sql
+++ migrations/00001_init.down.sql
@@ -1,5 +1,14 @@
 BEGIN;
 
-DROP TABLE IF EXISTS public.currency CASCADE;
+DROP TABLE IF EXISTS public.users CASCADE;
+DROP TABLE IF EXISTS public.cars CASCADE;
+DROP TABLE IF EXISTS public.car_model CASCADE;
+DROP TABLE IF EXISTS public.car_brand CASCADE;
+DROP TABLE IF EXISTS public.car_modification CASCADE;
+DROP TABLE IF EXISTS public.products CASCADE;
+DROP TABLE IF EXISTS public.product_category CASCADE;
+DROP TABLE IF EXISTS public.product_discount CASCADE;
+DROP TABLE IF EXISTS public.product_inventory CASCADE;
+DROP TABLE IF EXISTS public.products_for_cars CASCADE;
 
 END;
--- migrations/00001_init.up.sql
+++ migrations/00001_init.up.sql
@@ -9,10 +9,86 @@ SET search_path = public, extensions;
 SET default_tablespace = '';
 SET default_with_oids = FALSE;
 
--- EXTENSIONS --
-
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- TABLES --
+CREATE TABLE public.users
+(
+    id SERIAL PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    created_at TIMESTAMP,
+    visited_at TIMESTAMP,
+    is_admin BOOLEAN NOT NULL
+);
+
+CREATE TABLE public.cars
+(
+    id SERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    brand_id INT REFERENCES public.car_brand(id),
+    model_id INT REFERENCES public.car_model(id),
+    modification_id INT REFERENCES public.car_modification(id)
+);
+
+CREATE TABLE car_brand
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE car_model
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE car_modification
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE public.product
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    brand TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price BIGINT NOT NULL,
+    image_id UUID,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    category_id INT REFERENCES public.product_category(id),
+    discount_id INT REFERENCES public.product_discount(id),
+    inventory_id INT REFERENCES public.product_inventory(id),
+    CONSTRAINT positive_price CHECK (price > 0)
+);
+
+CREATE TABLE public.product_category
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE public.product_discount
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE public.product_inventory
+(
+    id SERIAL PRIMARY KEY,
+    quantity INT
+);
+
+CREATE TABLE products_for_cars
+(
+    product_id INT REFERENCES public.product(id),
+    car_id INT REFERENCES public.cars(id)
+);
 
 COMMIT;

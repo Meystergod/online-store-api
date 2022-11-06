8f88e7bece4cc9536fdf91dce4adeb119d4d7076
Meystergod <malahov_gleb@mail.ru>
2022-11-06 10:00:08 +0500

    update migrations

--- migrations/00001_init.down.sql
+++ migrations/00001_init.down.sql
@@ -1,11 +1,11 @@
 BEGIN;
 
-DROP TABLE IF EXISTS public.users CASCADE;
-DROP TABLE IF EXISTS public.cars CASCADE;
+DROP TABLE IF EXISTS public.user CASCADE;
+DROP TABLE IF EXISTS public.car CASCADE;
 DROP TABLE IF EXISTS public.car_model CASCADE;
 DROP TABLE IF EXISTS public.car_brand CASCADE;
 DROP TABLE IF EXISTS public.car_modification CASCADE;
-DROP TABLE IF EXISTS public.products CASCADE;
+DROP TABLE IF EXISTS public.product CASCADE;
 DROP TABLE IF EXISTS public.product_category CASCADE;
 DROP TABLE IF EXISTS public.product_discount CASCADE;
 DROP TABLE IF EXISTS public.product_inventory CASCADE;
--- migrations/00001_init.up.sql
+++ migrations/00001_init.up.sql
@@ -11,7 +11,7 @@ SET default_with_oids = FALSE;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE public.users
+CREATE TABLE public.user
 (
     id SERIAL PRIMARY KEY,
     password TEXT NOT NULL,
@@ -22,47 +22,31 @@ CREATE TABLE public.users
     is_admin BOOLEAN NOT NULL
 );
 
-CREATE TABLE public.cars
+CREATE TABLE public.car_brand
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE public.car_model
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE public.car_modification
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE public.car
 (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     brand_id INT REFERENCES public.car_brand(id),
     model_id INT REFERENCES public.car_model(id),
     modification_id INT REFERENCES public.car_modification(id)
-);
-
-CREATE TABLE car_brand
-(
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL
-);
-
-CREATE TABLE car_model
-(
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL
-);
-
-CREATE TABLE car_modification
-(
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL
-);
-
-CREATE TABLE public.product
-(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    brand TEXT NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    price BIGINT NOT NULL,
-    image_id UUID,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
-    category_id INT REFERENCES public.product_category(id),
-    discount_id INT REFERENCES public.product_discount(id),
-    inventory_id INT REFERENCES public.product_inventory(id),
-    CONSTRAINT positive_price CHECK (price > 0)
 );
 
 CREATE TABLE public.product_category
@@ -85,10 +69,27 @@ CREATE TABLE public.product_inventory
     quantity INT
 );
 
+CREATE TABLE public.product
+(
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price BIGINT NOT NULL,
+    image_id UUID,
+    specifications JSONB,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    category_id INT REFERENCES public.product_category(id),
+    discount_id INT REFERENCES public.product_discount(id),
+    inventory_id INT REFERENCES public.product_inventory(id),
+    CONSTRAINT positive_price CHECK (price > 0)
+);
+
 CREATE TABLE products_for_cars
 (
     product_id INT REFERENCES public.product(id),
-    car_id INT REFERENCES public.cars(id)
+    car_id INT REFERENCES public.car(id)
 );
 
 COMMIT;
